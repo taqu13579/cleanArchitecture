85951ebf478e5d32191cb1835418040b3b44cc0f
taqu13579 <92580230+taqu13579@users.noreply.github.com>
2025-06-15 15:50:02 +0900

    Fix date format and remove unused import

--- src/src/main/UserCreatePresenter.java
+++ src/src/main/UserCreatePresenter.java
@@ -7,7 +7,7 @@ public class UserCreatePresenter implements IUserCreatePresenter {
     public void Complete(UserCreateOutputData outputData) {
         String userId = outputData.userId;
         Date createdDate = outputData.created;
-        String createdDateText = new SimpleDateFormat("yyyy/mm/dd").format(createdDate);
+        String createdDateText = new SimpleDateFormat("yyyy/MM/dd").format(createdDate);
         var model = new UserCreateViewModel(userId, createdDateText);
         System.out.println(String.format("id:%s, created:%s", model.userId, model.createDate));
     }
